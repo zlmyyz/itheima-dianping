52ce70c2ec3034c4fcbadabf76ae11f471e9b987
therenoducks <3225534448@qq.com>
2026-05-09 11:42:34 +0800

    redis

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,8 +1,12 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements Ilock {
@@ -10,6 +14,13 @@ public class SimpleRedisLock implements Ilock {
     private String name;
     private StringRedisTemplate redisTemplate;
     private  static final String KEY_PREFIX = "lock:";
+
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT=new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     /*
     * 用UUID去做当前线程的标识，因为JVM用的自增逻辑，不同的JVM可能会出现线程ID一致的情况
@@ -33,15 +44,25 @@ public class SimpleRedisLock implements Ilock {
 
     @Override
     public void unlock() {
-        //获取线程标识
-        String thread = ID_PREFIX+Thread.currentThread().getId();
-        //获取锁中的标识
-        String ID = redisTemplate.opsForValue().get(KEY_PREFIX + name);
-        /*
-        * 释放锁的时候要去判断标识是不是一样，防止释放别人的锁
-        * */
-        if(thread.equals(ID)) {
-            redisTemplate.delete(KEY_PREFIX + name);
-        }
+        //调用lua脚本
+        redisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX+name),
+                ID_PREFIX+Thread.currentThread().getId()
+        );
     }
+
+//    @Override
+//    public void unlock() {
+//        //获取线程标识
+//        String thread = ID_PREFIX+Thread.currentThread().getId();
+//        //获取锁中的标识
+//        String ID = redisTemplate.opsForValue().get(KEY_PREFIX + name);
+//        /*
+//        * 释放锁的时候要去判断标识是不是一样，防止释放别人的锁
+//        * */
+//        if(thread.equals(ID)) {
+//            redisTemplate.delete(KEY_PREFIX + name);
+//        }
+//    }
 }
